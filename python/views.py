ef069b3df10103c0e8a1656a9e5bc11a426d0cf6
Paul Leroy Ducardonnoy <pld.pro@hotmail.com>
2021-09-24 10:35:18 +0200

    getJson

--- python/views.py
+++ python/views.py
@@ -1,10 +1,23 @@
 from flask import render_template
 import json
 
-books = [{"id": 1, "name": "Book 1"}, {"id": 2, "name": "Book 2"}]
+books = []
+
+fileName = './json/library.json'
 
 def index():
     return render_template('index.html')
 
 def get_books():
+  file = open(fileName, 'r')
+  data = json.load(file)
+  file.close()
+
+  with open(fileName) as file:
+      data = json.load(file)
+
+  for key in data.keys():
+    for book in data[key]:
+      books.append(book)
+
   return json.dumps(books)
